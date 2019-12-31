f53285a748d410a90304eeefe8943c81f3a08fa1
omar okla <omaro@me.com>
2019-12-30 20:57:46 -0500

    Picture creation and checking

--- bookSelection.py
+++ bookSelection.py
@@ -1,6 +1,9 @@
 import fitz
 import os
+import random
 
+inputFormat = ".pdf"
+outputFormat = ".png"
 path = './books'
 
 # for book in os.listdir('./books'):
@@ -11,10 +14,36 @@ bookPaths = []
 bookFiles = os.listdir(path)
 
 for book in bookFiles:
-    full_path = os.path.join(path, book)
-    # print(book)
-    # print(full_path)
-    bookPaths.append(full_path)
+    if (book[-4:] == inputFormat):
+        full_path = os.path.join(path, book)
+        bookPaths.append(full_path)
 
 
 print(bookPaths)
+
+bookToPrint = bookPaths[1]
+
+book = fitz.open(bookToPrint)
+totalPages = book.pageCount
+
+# print(totalPages)
+
+
+# books that don't have valid table of contents page selection process
+randomPageNumber = random.randint(15, totalPages-8)
+print(f"RANDOM: {randomPageNumber}")
+# end
+
+# doc = book.getToC()
+# pageNumber = 33
+pageNumber = randomPageNumber
+page = book.loadPage(pageNumber)
+picOfPage = page.getPixmap()
+output = f"{bookToPrint}-{pageNumber}.png"
+# print(doc)
+
+if os.path.isfile(output):
+    print("File already exists!")
+else:
+    print("Did not exist before. New upload!")
+    picOfPage.writePNG(output)
